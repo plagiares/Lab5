183ecd5ff018a31924b620cba81723f8f4a8028e
plagiares <plagiares@gmail.com>
2015-03-19 23:33:17 -0400

    Constructor of Point can't have negative values

--- Shape/point.cpp
+++ Shape/point.cpp
@@ -9,7 +9,7 @@ Point::Point( double _x, double _y ) :
 	x(_x), 
 	y(_y)
 {	
-	
+	if (x < 0 || y < 0) throw invalid_argument("localisations can't be negative");
 }
 
 bool Point::operator == (const Point & point) const
