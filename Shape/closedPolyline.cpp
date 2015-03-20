d1bb83660411c0318a09ae8aed58074c8988ed14
plagiares <plagiares@gmail.com>
2015-03-20 18:09:56 -0400

    ClosedPolyline is Finished (draw has been complet)

--- Shape/closedPolyline.cpp
+++ Shape/closedPolyline.cpp
@@ -9,7 +9,7 @@ ClosedPolyline::ClosedPolyline(IWindowAPI &_windowAPI)
 void ClosedPolyline::draw()
 {
 	windowAPI->setDrawingColor(lineColor);
-	if (point.size() < 2) throw runtime_error("Not enough points have been added");
+	if (point.size() < 3) throw runtime_error("Not enough points have been added");
 	windowAPI->drawLine(getPoint(0), getPoint(1));
 	windowAPI->drawLine(getPoint(1), getPoint(2));
 	windowAPI->drawLine(getPoint(2), getPoint(0));
