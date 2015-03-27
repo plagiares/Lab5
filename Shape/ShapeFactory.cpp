442a08ea172edc2f6373e323eddff43e364cb929
plagiares <plagiares@gmail.com>
2015-03-27 13:38:11 -0400

    ShapeFactory

--- Shape/ShapeFactory.cpp
+++ Shape/ShapeFactory.cpp
@@ -1,5 +1,51 @@
 #include "stdafx.h"
 
 using namespace ShapeLibrary;
+#include "stdafx.h"
+#include "shapeFactory.h"
 
+
+ShapeFactory::ShapeFactory(IWindowAPI& _windowAPI)
+{
+	windowAPI = &_windowAPI;
+}
+
+ShapeFactory::~ShapeFactory()
+{
+}
+
+Shape& ShapeFactory::createOpenPolyLine()
+{
+	Shape* openPolyLine = new OpenPolyline(*windowAPI);
+	return *openPolyLine;
+}
+
+Shape& ShapeFactory::createCircle(Point _point, int _rayon)
+{
+	Circle* circle = new Circle(*windowAPI);
+	circle->setCenter(_point);
+	circle->setRadius(_rayon);
+	return *circle;
+}
+
+Shape& ShapeFactory::createRectangle(Point _point, int _height, int _width)
+{
+	Rectangle* rectangle = new Rectangle(*windowAPI);
+	rectangle->setPosition(_point);
+	rectangle->setHeight(_height);
+	rectangle->setWidth(_width);
+	return *rectangle;
+}
+
+Shape& ShapeFactory::createClosedPolyLine()
+{
+	Shape* closedPolyLine = new ClosedPolyline(*windowAPI);
+	return *closedPolyLine;
+}
+
+Shape& ShapeFactory::createPolygone()
+{
+	Polygon* polygon = new Polygon(*windowAPI);
+	return *polygon;
+}
 
--- Shape/shapeFactory.h
+++ Shape/shapeFactory.h
@@ -3,10 +3,19 @@
 
 namespace ShapeLibrary
 {
-	class	ShapeFactory
+	class ShapeFactory
 	{
 	public:
+		ShapeFactory(IWindowAPI& _windowAPI);
+		~ShapeFactory();
 
-		
+		Shape& createOpenPolyLine();
+		Shape& createCircle(Point _point, int _rayon);
+		Shape& createRectangle(Point _point, int _height, int _width);
+		Shape& createClosedPolyLine();
+		Shape& createPolygone();
+
+	private:
+		IWindowAPI* windowAPI;
 	};
 }
