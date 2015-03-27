ca7330708762beadba8a9185eeebc1297023cf3e
plagiares <plagiares@gmail.com>
2015-03-27 13:30:26 -0400

    Creation of windowRender

--- AnimationRender/WindowRender.cpp
+++ AnimationRender/WindowRender.cpp
@@ -3,3 +3,50 @@
 using namespace WindowRender;
 using namespace ShapeLibrary;
 
+WindowsRender::WindowsRender(IWindowAPI& _windowAPI)
+{
+	windowAPI = &_windowAPI;
+}
+
+WindowsRender::~WindowsRender()
+{
+}
+
+void WindowsRender::attach(Shape& _shape)
+{
+	shapes.push_back(&_shape);
+}
+
+void WindowsRender::render()
+{
+	bool quit = false;
+	while (!quit)
+	{
+		windowAPI->clearScreen();
+		for (int i = 0; i < shapes.size(); i++)
+		{
+			shapes[i]->draw();
+		}
+		if (windowAPI->hasEvent())
+		{
+			if (windowAPI->getEvent().getEventType() == QUIT)
+			{
+				quit = true;
+			}
+		}
+		windowAPI->displayScreen();
+		windowAPI->wait(1000 / 60);
+	}
+}
+
+void WindowsRender::putOnTop(Shape& _shape)
+{
+	for (int i = 0; i < shapes.size(); i++)
+	{
+		if (shapes[i] == &_shape)
+		{
+			shapes.erase(shapes.begin() + i);
+		}
+	}
+	shapes.push_back(&_shape);
+}
--- AnimationRender/WindowRender.h
+++ AnimationRender/WindowRender.h
@@ -8,8 +8,16 @@ namespace WindowRender
 {
 	class WindowsRender
 	{
-	
-		
+	public:
+		WindowsRender(IWindowAPI& _windowAPI);
+		~WindowsRender();
 
+		void attach(Shape& _shape);
+		void render();
+		void putOnTop(Shape& _shape);
+
+	private:
+		IWindowAPI* windowAPI;
+		vector<Shape*> shapes;
 	};
 }
